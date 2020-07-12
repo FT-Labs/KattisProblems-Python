dd77aa1e35a05770eb46034d3040fe30b2d06398
ardaPhysTech <phystecharda@gmail.com>
2020-07-12 22:28:36 +0300

    New solutions added.

---
+++ deathknight.py
@@ -0,0 +1,12 @@
+"""
+Author : Arda
+Date : 7/12/2020
+"""
+
+tot = 0
+
+for _ in range(int(input())):
+    if "CD" not in input():
+        tot += 1
+
+print(tot)
--- prva.py
+++ prva.py
@@ -12,36 +12,43 @@ for i in range(len(ascii_lowercase)):
 
 r,c = [int(x) for x in input().split()]
 
-m = [["" for x in range(r)] for j in range(c)]
+m = [["" for x in range(c)] for j in range(r)]
 
 ans = dict()
 
 for _ in range(r):
     x = input()
 
-    for i in range(len(x)):
+    for i in range(c):
         m[_][i] = x[i]
 
+arr = []
+
 for row in m:
-    s = ""
-    for q in row:
-        if q!= "#":
-            s += q
-        elif row[-1] == q and q!= "#":
-            tot = 0
+     s = ""
+     for ch in row:
+         if ch!="#":
+             s += ch
+         else:
+             if len(s)>=2:
+                 arr.append(s)
+             s = ""
+     if len(s)>=2: arr.append(s)
 
-            for i in row:
-                tot += dc[i]
-            ans[s] = tot
-        else:
-            if len(s)>=2:
-                tot = 0
-                for i in row:
-                    tot += dc[i]
-                ans[s] = tot
-            s = ""
+try:
+    for i in range(r):
+        s = ""
+        for ch in [row[i] for row in m]:
+            if ch != "#":
+                s += ch
+            else:
+                if len(s) >= 2:
+                    arr.append(s)
+                s = ""
+        if len(s) >=2 : arr.append(s)
+except:
+    pass
 
-print(ans)
-for i in range(r):
-    print([row[i] for row in m])
+
+print(sorted(arr)[0])
 
